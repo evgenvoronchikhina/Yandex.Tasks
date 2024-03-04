e235ec5b2fb1b87d74eab3ffec31e85e4b3204a5
Rustam Gapsatarov <rust.gapsatarov123@gmail.com>
2024-03-04 23:12:27 +0300

    added elasticsearch to docker

--- movies_admin/api/v1/urls.py
+++ movies_admin/api/v1/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from movies_admin.api.v1 import views
 
 urlpatterns = [
-    path('movies/', views.MoviesListApi.as_view())
+    path('movies/', views.MoviesListApi.as_view()),
+    path('movies/<uuid:pk>/', views.MoviesDetailApi.as_view())
 ]
--- movies_admin/api/v1/views.py
+++ movies_admin/api/v1/views.py
@@ -1,9 +1,33 @@
+from django.contrib.postgres.aggregates import ArrayAgg
+from django.db.models import Q
 from django.http import JsonResponse
-from django.views import View
+from django.views.generic.list import BaseListView
+from django.core import serializers
+from django.forms.models import model_to_dict
 
-class MoviesListApi(View):
-    http_method_names = ['get']
+from movies_admin.models import Filmwork
 
-    def get(self, request, *args, **kwargs):
-        # Получение и обработка данных
-        return JsonResponse({})
+
+class MoviesListApi(BaseListView):
+    model = Filmwork
+    paginate_by = 50
+    http_method_names = ['get']  # Список методов, которые реализует обработчик
+    
+    def get_queryset(self):
+        return self.model.objects.values()# Сформированный QuerySet
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        queryset = self.get_queryset()
+        paginator, page, queryset, is_paginated = self.paginate_queryset(
+            queryset, 
+            self.paginate_by
+        )
+        return {'count': paginator.count, 
+            'total_pages':paginator.count // self.paginate_by + (paginator.count % self.paginate_by > 0),
+            'prev':1,
+            'next':2,
+            'results':list(queryset)
+        } 
+
+    def render_to_response(self, context, **response_kwargs):
+        return JsonResponse(context)
